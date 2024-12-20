e825a3dad42867203e36df370039505d941ce840
meltingsemicolon <meltingsemicolon@gmail.com>
2024-12-20 18:51:58 +0545

    worst fit page replacement algorithm

---
+++ lab5/worstfit.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#define maxsize 10
+int main()
+{
+    int partitions[maxsize], partitionSize[maxsize], allocation[maxsize], wastage[maxsize];
+    int numPartitions, numProcesses;
+
+    printf("Enter the number of partitions: ");
+    scanf("%d", &numPartitions);
+
+    printf("Enter the sizes of the partitions:\n");
+    for (int i = 0; i < numPartitions; i++)
+    {
+        printf("Partition %d: ", i + 1);
+        scanf("%d", &partitions[i]);
+        partitionSize[i] = partitions[i]; // Store the original sizes for display later
+        allocation[i] = -1;               // Initialize all allocations to -1, meaning no process is allocated
+    }
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &numProcesses);
+
+    int processes[maxsize];
+    printf("Enter the sizes of the processes:\n");
+    for (int i = 0; i < numProcesses; i++)
+    {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &processes[i]);
+    }
+
+    // Worst Fit Allocation
+    for (int i = 0; i < numProcesses; i++)
+    {
+        int worstIdx = -1;
+
+        for (int j = 0; j < numPartitions; j++)
+        {
+            if (partitions[j] >= processes[i])
+            {
+                if (worstIdx == -1 || partitions[j] > partitions[worstIdx])
+                {
+                    worstIdx = j;
+                }
+            }
+        }
+
+        // If a suitable partition is found
+        if (worstIdx != -1)
+        {
+            allocation[worstIdx] = i;                 // Allocate process to this partition
+            partitions[worstIdx] -= processes[i];     // Reduce available partition size
+            wastage[worstIdx] = partitions[worstIdx]; // Calculate wastage
+        }
+    }
+      // display results
+    for (int i = 0; i < numPartitions; i++)
+    {
+        printf("  %d\t", partitionSize[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < numPartitions; i++)
+    {
+        if (allocation[i] != -1)
+        {
+            printf("| %d\t", allocation[i] + 1);
+        }
+        else
+        {
+            printf("| -\t");
+        }
+    }
+    int i = 0;
+    int sum = 0;
+    while (i < numPartitions)
+    {
+        if (allocation[i] != -1)
+        {
+            sum += wastage[i];
+        }
+        i++;
+    }
+    printf("|\nTotal wastage =%dKB", sum);
+    return 0;
+}
